7c39b81c8fd506c4204aab13d096dc05d68b68cb
Maria <mariya-ivanova@list.ru>
2024-11-02 10:04:37 +0300

    the solution was updated

--- task2.py
+++ task2.py
@@ -1,13 +1,15 @@
 task_count = 0
+task_sum = 0
 go_to_store = False
-i = 1
-while i < 9:
-    task_count += i
+i = 0
+while i != 8:
+    i += 1
+    task_count = int(input('Сколько задач выполнил Максим за этот час?'))
+    task_sum += task_count
     get_call = int(input('Звонит жена. Взять трубку?'))
     if get_call == 1:
         go_to_store = True
-    i += 1
 
-print('Рабочий день закончился. Всего выполнено задач: ', task_count)
-if go_to_store == True:
+print('Рабочий день закончился. Всего выполнено задач: ', task_sum)
+if go_to_store:
     print('Нужно зайти в магазин')
