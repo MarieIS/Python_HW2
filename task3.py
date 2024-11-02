873431f56ec24afcafea6a390a6b55675a671568
Maria <mariya-ivanova@list.ru>
2024-11-02 10:23:59 +0300

    the solution was updated

--- task3.py
+++ task3.py
@@ -3,9 +3,9 @@ try_count = 0
 success = False
 while not success:
     your_num = int(input('Введите ваше число: '))
+    try_count += 1
     if your_num == my_num:
-        try_count += 1
         success = True
         print('Количество попыток: ', try_count)
     else:
-        try_count += 1
+        success = False
